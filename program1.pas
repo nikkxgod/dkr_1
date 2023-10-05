cda403b3b5e9de44f55fe533e5921d43cb1574e2
nikkxgod <100446613+nikkxgod@users.noreply.github.com>
2023-10-05 08:40:45 +0300

    Add files via upload

---
+++ program1.pas
@@ -0,0 +1,12 @@
+﻿var e,x:real;otvet:real;
+  begin
+    e:=2.71;
+    writeln('Введите х');
+    read(x);
+    if (x<-8) then otvet:=3/Power(e,x);
+    if (x>=-8) and (x<0) then otvet:=Power(x,0.1*x)/sin(x)-(ln(x)/ln(10))/25;
+    if (x>=0) and (x<5) then otvet:=cos(x);
+    if (5<=x) then otvet:=cos(x)+39;
+    if (otvet<>otvet) then writeln('При х равному ',x,', функция не имеет значения')
+    else writeln('При х равному ',x,', функция равна ', otvet:0:2);
+  end.
---
+++ program2.pas
@@ -0,0 +1,14 @@
+﻿var e,x,i:real;otvet:real;
+  begin
+    e:=2.71;
+    x:=-10;
+    while x<=7 do begin
+    if (x<-8) then otvet:=3/Power(e,x);
+    if (x>=-8) and (x<0) then otvet:=Power(x,0.1*x)/sin(x)-(ln(x)/ln(10))/25;
+    if (x>=0) and (x<5) then otvet:=cos(x);
+    if (5<=x) then otvet:=cos(x)+39;
+    if (otvet<>otvet) then writeln('При х равному ',x:0:2,', функция не имеет значения')
+    else writeln('При х равному ',x:0:2,', функция равна ', otvet:0:2);
+    x:=x+0.1;
+    end;
+  end.
